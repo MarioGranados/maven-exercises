d914d1d110fccf3a93e0599bb2f679995cc908db
Mario Granados <mario.alberto.granados1@gmail.com>
2022-03-08 15:28:02 -0600

    pushing folder structure

---
+++ src/main/java/Example.java
@@ -0,0 +1,16 @@
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
+public class Example {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter a word so that I could reverse it");
+        String str = in.nextLine();
+
+
+        System.out.println(StringUtils.reverse(str));
+    }
+}
